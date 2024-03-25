ee8efed703de4cc344d72fef36e0a0dbe75719ce
seesharpcode <seesharpcode@gmail.com>
2024-03-24 19:20:14 -0500

    improved card styles

--- src/character-list/CharacterCard.tsx
+++ src/character-list/CharacterCard.tsx
@@ -1,13 +1,15 @@
-import { Anchor, Badge, Card, Group, Image, Stack, Text, Title } from '@mantine/core';
+import { Anchor, AspectRatio, Badge, Card, Group, Image, Stack, Text, Title } from '@mantine/core';
 import { IconHeartFilled } from '@tabler/icons-react';
 import { Link } from 'react-router-dom';
 import { Character, getSkills } from '../shared/character';
 
 export default function CharacterCard({ character }: { character: Character }) {
   return (
-    <Card shadow="sm" padding="md" radius={0} style={{ display: 'flex', flexDirection: 'column' }} withBorder>
+    <Card shadow="sm" padding="md" radius={0} withBorder>
       <Card.Section>
-        <Image src={character.media.image} alt="Norway" />
+        <AspectRatio ratio={1280 / 720}>
+          <Image src={character.media.image} alt="Norway" />
+        </AspectRatio>
       </Card.Section>
       <Group justify="space-between" mt="xs">
         <Title order={5}>{character.name}</Title>
@@ -23,22 +25,22 @@ export default function CharacterCard({ character }: { character: Character }) {
       <Text size="sm" lineClamp={8}>
         {character.summary}
       </Text>
-      <Stack style={{ flexGrow: 1, justifyContent: 'flex-end' }}>
-        <Group mt="xs">
-          {Array.from(new Set(getSkills(character))).map(skill => (
-            <Badge size="sm" key={skill}>
-              {skill}
-            </Badge>
-          ))}
+
+      <Group mt="sm" gap="xs">
+        {Array.from(new Set(getSkills(character))).map(skill => (
+          <Badge size="sm" key={skill}>
+            {skill}
+          </Badge>
+        ))}
+      </Group>
+
+      <Group mt="xs" justify="space-between">
+        <Link to={`/characters/${character.id}`}>View</Link>
+        <Group>
+          <IconHeartFilled size="1rem" />
+          <Text>334</Text>
         </Group>
-        <Group justify="space-between">
-          <Link to={`/characters/${character.id}`}>View</Link>
-          <Group>
-            <IconHeartFilled size="1rem" />
-            <Text>334</Text>
-          </Group>
-        </Group>
-      </Stack>
+      </Group>
     </Card>
   );
 }
